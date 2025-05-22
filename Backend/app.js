e64b87b14f327e1383f8a0b1c66bec70bc51b855
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-05-22 19:02:49 +0530

    modularising our code

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import {nanoid} from 'nanoid';
 import dotenv from 'dotenv';
 import connectDB from './src/config/mongo.config.js';
+import urlSchema from './src/models/short_url.model.js';
+import short_url from './src/routes/short_url.routes.js';
 
 dotenv.config("./.env");
 
@@ -11,11 +13,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.post('/api/create', (req, res) => {
-    const {url} = req.body;
-    console.log(url);
-    res.send(nanoid(7));
-});
+app.post('/api/create', short_url);
+
+app.get('/:id', async (req,res)=>{
+    const {id} = req.params;
+    const url = await urlSchema.findOne({short_url: id});
+    if(url){
+        res.redirect(url.full_url);
+    }else{
+        res.status(404).send('URL not found');
+    }
+})
 
 app.listen(3000, () => {
     connectDB();
---
+++ Backend/src/controller/short_url.controller.js
@@ -0,0 +1,9 @@
+export const createShortUrl = async (req, res) => {
+    const {url} = req.body;
+    const shortUrl = nanoid(7);
+    const newUrl = new urlSchema({
+        full_url: url,
+        short_url: shortUrl,
+    });
+    newUrl.save();
+}
